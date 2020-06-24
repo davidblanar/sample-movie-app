d578b6eed7b0500f23451e9df97b7ace68732cad
davidblanar <david.blanar@gmail.com>
2020-06-24 12:48:03 +0200

    player

---
+++ src/components/Player.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import Video from "react-native-video";
+
+const VIDEO_URL =
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+const Player: React.FC = () => {
+  return (
+    <Video source={{ uri: VIDEO_URL }} controls={true} style={styles.player} />
+  );
+};
+
+const styles = StyleSheet.create({
+  player: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: "#000"
+  }
+});
+
+export default Player;
--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteProp } from "@react-navigation/core";
 import { Routes, StackParamList } from "../components/Navigator";
 import { useRoute } from "@react-navigation/native";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 import MovieBgImage from "../components/MovieBgImage";
+import Player from "../components/Player";
 
 type RouteProps = RouteProp<StackParamList, Routes.DETAIL>;
 
 const Detail: React.FC = () => {
   const route = useRoute<RouteProps>();
   const { movie } = route.params;
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  const onPress = () => setShowPlayer(true);
+
   return (
-    <View style={styles.view}>
+    <ScrollView contentContainerStyle={styles.view}>
       <MovieBgImage movie={movie} />
       <View style={styles.innerView}>
         <Text>{movie.overview}</Text>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
           <Text style={styles.buttonText}>PLAY</Text>
         </TouchableOpacity>
       </View>
-    </View>
+      {showPlayer && <Player />}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  view: { flex: 1, alignItems: "center", backgroundColor: "#ffffff" },
-  innerView: { padding: 20, flex: 1, alignItems: "center" },
+  view: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#ffffff"
+  },
+  innerView: {
+    padding: 20,
+    flex: 1,
+    alignItems: "center"
+  },
   button: {
     width: 150,
     marginTop: 20,
