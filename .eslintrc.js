6d1d89f3f0c8feb2011d5a0b64fbdb708cd151f8
davidblanar <david.blanar@gmail.com>
2020-06-24 10:10:13 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["@react-native-community"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "no-only-tests"],
+  rules: {
+    quotes: "off",
+    "comma-dangle": "off",
+    radix: "off",
+    "no-only-tests/no-only-tests": "error"
+  },
+  env: {
+    jest: true
+  }
 };
